61ae1600339784d09d3fbc00820bb7830e5a87cd
deepanshuthappa <deepanshuthappa01@gmail.com>
2024-07-23 19:40:30 +0530

    Initial commit1

---
+++ Keylogger/__init__.py
@@ -0,0 +1,9 @@
+"""
+Keylogger Package
+
+This package provides a keylogger implementation using the pynput library.
+"""
+
+from .keylogger import Keylogger
+
+__all__ = ['Keylogger']
--- Keylogger/init__.py
+++
@@ -1,4 +0,0 @@
-# __init__.py
-from .keylogger import Keylogger
-
-__all__ = ['Keylogger']
